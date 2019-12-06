c1d97a8a6235d82c9175e879ddd299b1f99d22d2
toly <toly.sukhotin@gmail.com>
2019-12-06 16:51:42 +0200

    hw1

--- age_to_bucket_parser.py
+++ age_to_bucket_parser.py
@@ -12,6 +12,21 @@ def read_json(json_file_path):
     return data
 
 
+def combine_people_by_ages(buckets_list, ppl_ages_list):
+    min_age = 0
+    max_age = ppl_ages_list[-1][1]  # the value of the last element in the sorted list
+    buckets_list.insert(len(buckets_list), max_age)  # add to bucket of ages the maximum age
+    result = []
+    for num in buckets_list:
+        names = []
+        for name, age in ppl_ages_list:
+            if age in range(min_age, num):
+                names.append(name)  # add names of people with age in range
+        result.append({str(min_age) + "-" + str(num): names})  # combine age ranges with names in list of dicts
+        min_age = num
+    return result
+
+
 def write_yaml(yaml_file_path, data_list):
     with open(yaml_file_path, "w") as yaml_file:
         f = yaml.dump(data_list, yaml_file, explicit_start=True, allow_unicode=True)
@@ -24,18 +39,8 @@ def main():
     if json_data:
         buckets = sorted(json_data["buckets"])  # sort buckets
         ppl_ages = sorted(json_data["ppl_ages"].items(), key=lambda x: x[1])  # sort peoples by age
-        min_age = 0
-        max_age = ppl_ages[-1][1]  # the value of the last element in the sorted list
-        buckets.insert(len(buckets), max_age)  # add to bucket of ages the maximum age
-        ppl_ages_ranges_list = []
-        for num in buckets:
-            names = []
-            for name, age in ppl_ages:
-                if age in range(min_age, num):
-                    names.append(name)  # add names of people with age in range
-            ppl_ages_ranges_list.append({str(min_age) + "-" + str(num): names})  # combine age ranges with names in list
-            min_age = num
-        write_yaml(yaml_file_path, ppl_ages_ranges_list)    # create result yaml file
+        ppl_ages_ranges_list = combine_people_by_ages(buckets, ppl_ages)
+        write_yaml(yaml_file_path, ppl_ages_ranges_list)  # create result yaml file
 
 
 if __name__ == '__main__':
