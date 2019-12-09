c5e791a1a5b73ee4c0bb20747a3b455d895f8f5c
Carmit Danon <carmitbe1@gmail.com>
2019-12-09 07:42:02 +0200

    replace global

--- age_to_bucket_parser.py
+++ age_to_bucket_parser.py
@@ -1,65 +1,55 @@
-import pprint
 import json
-people = {}
-buckets = {}
-result = {}
-minimum_age = 0
-maximum_age = 100
+
 
 def get_data():
-    global people
-    global buckets
     with open('hw.json') as input_file:
         data = json.load(input_file)
     people = data["ppl_ages"]
     buckets = data["buckets"]
     buckets.sort()
+    return people, buckets
 
-def create_buckets():
-    global result
-    global maximum_age
-    min = minimum_age
-    max = maximum_age
+
+def create_buckets(buckets, minimum, maximum):
     t = {}
     for bucket_age in buckets:
-        range = str(min) + "-" + str(bucket_age)
+        range = str(minimum) + "-" + str(bucket_age)
         t[range] = []
-        min = bucket_age+1
-    last_range = str(buckets[-1]+1) + "-" + str(maximum_age)
+        minimum = bucket_age+1
+    last_range = str(buckets[-1]+1) + "-" + str(maximum)
     t[last_range] = []
-
-
     result = dict(t)
+    return result
 
-def get_max_age():
-    global people
-    global maximum_age
+
+def get_max_age(people):
     max_age = max(zip(people.values(), people.keys()))[0]
     maximum_age = int(max_age)
+    return maximum_age
 
-def group_people_by_age():
-    global result
-    global people
-    #bucket_data = []
-    #print(people)
+
+def group_people_by_age(people,result):
     for person,age in people.items():
         for key in result:
             if int(age) >= int(key.split('-')[0]) and int(age) < int(key.split('-')[1]):
                 result[key].append(person)
+    return result
 
-def print_result():
-    global result
+
+def print_result(result):
     for key,value in result.items():
         print(key + ":")
         for pers in value:
             print("-  " + pers)
 
+
 def main():
-    get_data()
-    get_max_age()
-    create_buckets()
-    group_people_by_age()
-    print_result()
+    minimum_age = 0
+    people, buckets = get_data()
+    maximum_age = get_max_age(people)
+    result = create_buckets(buckets, minimum_age, maximum_age)
+    result = group_people_by_age(people, result)
+    print_result(result)
 
 
 if __name__ == '__main__':
